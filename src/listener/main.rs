9fd461dc57cf677bf98872888024df2e348180d4
Hand-of-Cthulhu <3rdhandofcthulhu@gmail.com>
2018-04-28 19:16:27 +1200

    Udp Test

--- src/listener/main.rs
+++ src/listener/main.rs
@@ -1,26 +1,22 @@
 extern crate enigo;
 
-use std::net::TcpListener;
+use std::net::UdpSocket;
 use std::io::Read;
 
 use enigo::{Enigo, KeyboardControllable, Key};
 
 fn main() {
-    let listener = TcpListener::bind("0.0.0.0:13111").expect("Error binding to port: ");
-    let mut data= [0u8; 2];
+    let listener = UdpSocket::bind("0.0.0.0:13111").expect("Error binding to port: ");
+    let mut data = [0u8; 2];
     let mut enigo = Enigo::new();
 
-    for stream in listener.incoming() {
-        let mut stream = stream.unwrap();
-        loop {
-            stream.read(&mut data).unwrap();
-            println!("Data: {}", data[0] as char);
-            if data[1] == 0 {
-                enigo.key_up(Key::Layout(data[0] as char))
-            } else {
-                enigo.key_down(Key::Layout(data[0] as char))
-            }
-            //enigo.key_click(Key::Layout(data[0] as char))
+    loop {
+        listener.recv(&mut data).unwrap();
+        println!("Data: {}", data[0] as char);
+        if data[1] == 0 {
+            enigo.key_up(Key::Layout(data[0] as char))
+        } else {
+            enigo.key_down(Key::Layout(data[0] as char))
         }
     }
 }
--- src/server/main.rs
+++ src/server/main.rs
@@ -1,16 +1,15 @@
 extern crate user32;
 
-use std::net::TcpStream;
+use std::net::UdpSocket;
 use std::io::Write;
-//use std::io::Read;
 
 fn main() {
-    let mut stream = TcpStream::connect("192.168.1.67:13111").expect("Can't connect to port: ");
-    //let mut stdin = std::io::stdin();
-    //let mut line_buffer = String::new();
+    let mut stream = UdpSocket::bind("127.0.0.1:13112").expect("Can't connect to port: ");
 
     let mut previous_key_state: Vec<bool> = Vec::new();
     previous_key_state.resize(255, false);
+    stream.connect("192.168.1.67:13111").expect("Failed to connect");
+//    stream.connect("127.0.0.1:13111").expect("Failed to connect");
     loop {
         std::thread::sleep(std::time::Duration::from_millis(20));
 
@@ -23,15 +22,12 @@ fn main() {
                 previous_key_state[i as usize] = is_current_key_down;
                 match i {
                     0x30 ... 0x5A => {
-                        stream.write( &[i, is_current_key_down as u8]).expect("Failed to write");
+                        stream.send(&[i, is_current_key_down as u8]).expect("Failed to write");
                     },
                     _ => {}
                 }
             }
         }
-//        line_buffer.clear();
-//        stdin.read_line(&mut line_buffer);
-//        stream.write( line_buffer.trim().as_bytes()).expect("Failed to write");
     }
 
 }
