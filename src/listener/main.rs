bb71594e447e1f23c279e190185c768b8464923d
Hand of Cthulhu <3rdhandofcthulhu@gmail.com>
2018-04-28 18:24:44 +1200

    Client key up down update

--- src/listener/main.rs
+++ src/listener/main.rs
@@ -7,7 +7,7 @@ use enigo::{Enigo, KeyboardControllable, Key};
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:13111").expect("Error binding to port: ");
-    let mut data= [0u8];
+    let mut data= [0u8; 2];
     let mut enigo = Enigo::new();
 
     for stream in listener.incoming() {
@@ -15,7 +15,12 @@ fn main() {
         loop {
             stream.read(&mut data).unwrap();
             println!("Data: {}", data[0] as char);
-            enigo.key_click(Key::Layout(data[0] as char))
+            if data[1] == 0 {
+                enigo.key_up(Key::Layout(data[0] as char))
+            } else {
+                enigo.key_down(Key::Layout(data[0] as char))
+            }
+            //enigo.key_click(Key::Layout(data[0] as char))
         }
     }
 }
