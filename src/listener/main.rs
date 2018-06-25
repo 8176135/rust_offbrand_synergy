e4b50184e98d9e372fec181b27e54f59c749721d
Hand-of-Cthulhu <3rdhandofcthulhu@gmail.com>
2018-06-25 19:32:14 +1200

    Backup commit

--- src/listener/main.rs
+++ src/listener/main.rs
@@ -1,22 +1,83 @@
 extern crate enigo;
 
+mod win_key_codes;
+
+use win_key_codes::*;
 use std::net::UdpSocket;
 use std::io::Read;
 
-use enigo::{Enigo, KeyboardControllable, Key};
+use enigo::{Enigo, KeyboardControllable, Key, MouseButton};
 
 fn main() {
     let listener = UdpSocket::bind("0.0.0.0:13111").expect("Error binding to port: ");
     let mut data = [0u8; 2];
-    let mut enigo = Enigo::new();
+    let enigo = Enigo::new();
 
     loop {
         listener.recv(&mut data).unwrap();
-        println!("Data: {}", data[0] as char);
-        if data[1] == 0 {
-            enigo.key_up(Key::Raw(data[0] as u16))
-        } else {
-            enigo.key_down(Key::Raw(data[0] as u16))
+        println!("Code: {:X}", data[0]);
+
+        if data[0] < 8 { // Mouse event
+            let mouse_btn_to_press = match data[0] {
+                VK_LBUTTON => MouseButton::Left,
+                VK_RBUTTON => MouseButton::Right,
+                VK_MBUTTON => MouseButton::Middle,
+                _ => continue
+            };
+            if data[1] == 0 {
+                println!("Click: {:?}", data[0]);
+                //enigo.mouse_down(mouse_btn_to_press);
+            } else {
+                println!("Up: {:?}", mouse_btn_to_press);
+                //enigo.mouse_up(mouse_btn_to_press);
+            }
+        } else { // or keyboard event
+            let key_to_press = match data[0] {
+                0x30...0x5A => Key::Layout(data[0] as char),
+                VK_NUMPAD0...VK_NUMPAD9 => Key::Layout((data[0] - 0x30) as char),
+                VK_SPACE => Key::Space,
+                VK_BACK => Key::Backspace,
+                VK_TAB => Key::Tab,
+                VK_ESCAPE => Key::Escape,
+                VK_RETURN => Key::Return,
+                VK_LEFT => Key::LeftArrow,
+                VK_RIGHT => Key::RightArrow,
+                VK_UP => Key::UpArrow,
+                VK_DOWN => Key::DownArrow,
+                VK_SHIFT => Key::Shift,
+                VK_CONTROL => Key::Control,
+                VK_LMENU => Key::Alt,
+                VK_HOME => Key::Home,
+
+                VK_DELETE => Key::Raw(83), // Linux only
+                VK_PRIOR => Key::PageUp,
+                VK_NEXT => Key::PageDown,
+                //VK_OEM_1 => Key::Layout('['),
+                VK_OEM_2 => Key::Layout('/'),
+                VK_OEM_3 => Key::Layout('`'),
+                VK_OEM_4 => Key::Layout('['),
+                VK_OEM_5 => Key::Layout('\\'),
+                VK_OEM_6 => Key::Layout(']'),
+                VK_OEM_7 => Key::Layout('\''),
+                VK_OEM_PLUS => Key::Layout('+'),
+                VK_OEM_MINUS => Key::Layout('-'),
+                VK_OEM_COMMA => Key::Layout(','),
+                VK_OEM_PERIOD => Key::Layout('.'),
+                _ => {
+                    println!("Key code {:X} not supported", data[0]);
+                    continue;
+                }
+            };
+
+//        println!("Data: {:?}", key_to_press);
+            if data[1] == 0 {
+//                enigo.key_click()
+                println!("Up: {:?}", key_to_press);
+//            enigo.key_up(key_to_press)
+            } else {
+                println!("Down: {:?}", key_to_press);
+//            enigo.key_down(key_to_press)
+            }
         }
     }
 }
--- src/server/main.rs
+++ src/server/main.rs
@@ -1,34 +1,101 @@
 extern crate user32;
+extern crate winapi;
+extern crate winit;
 
 use std::net::UdpSocket;
+use winit::os::windows::WindowExt;
 use std::io::Write;
 
+use winit::{Event, WindowEvent, VirtualKeyCode, ElementState};
+
 fn main() {
-    let mut stream = UdpSocket::bind("0.0.0.0:13112").expect("Can't connect to port: ");
+    let mut events_loop = winit::EventsLoop::new();
+    let window = winit::WindowBuilder::new()
+        .build(&events_loop).unwrap();
+
+    window.set_title("A fantastic window!");
+    window.set_cursor_state(winit::CursorState::Hide).unwrap();
+
+    let stream = UdpSocket::bind("0.0.0.0:13112").expect("Can't connect to port: ");
 
     let mut previous_key_state: Vec<bool> = Vec::new();
     previous_key_state.resize(255, false);
-    stream.connect("192.168.1.67:13111").expect("Failed to connect");
-//    stream.connect("127.0.0.1:13111").expect("Failed to connect");
-    loop {
-        std::thread::sleep(std::time::Duration::from_millis(20));
+    //stream.connect("192.168.1.72:13111").expect("Failed to connect");
+    stream.connect("127.0.0.1:13111").expect("Failed to connect");
 
-        for i in 8..255u8 {
-            let mut is_current_key_down: bool;
-            unsafe {
-                is_current_key_down = user32::GetAsyncKeyState(i as i32) & (1 << 15) != 0;
-            }
-            if previous_key_state[i as usize] != is_current_key_down {
-                previous_key_state[i as usize] = is_current_key_down;
-                stream.send(&[i, is_current_key_down as u8]).expect("Failed to write");
-//                match i {
-//                    0x30 ... 0x5A => {
-//                        stream.send(&[i, is_current_key_down as u8]).expect("Failed to write");
-//                    },
-//                    _ => {}
-//                }
-            }
+//    let is_in_another_window = std::sync::Arc::new(std::sync::Mutex::new(42));
+//    let clone_thing = is_in_another_window.clone();
+//    std::thread::spawn(move || other_thread(clone_thing));
+
+    events_loop.run_forever(|event| {
+//        println!("{:?}", event);
+//
+//        {
+//            let mut data = is_in_another_window.lock().unwrap();
+//            *data += 1;
+//        }
+
+        window.set_cursor_position(100, 100).expect("Error setting cursor pos");
+//        for i in 1..255u8 {
+//            let mut is_current_key_down: bool;
+//            unsafe {
+//                is_current_key_down = user32::GetAsyncKeyState(i as i32) & (1 << 15) != 0;
+//            }
+//            if previous_key_state[i as usize] != is_current_key_down {
+//                previous_key_state[i as usize] = is_current_key_down;
+//                stream.send(&[i, is_current_key_down as u8]).expect("Failed to write");
+//            }
+//        }
+        match event {
+            Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => winit::ControlFlow::Break,
+            Event::WindowEvent { event: WindowEvent::KeyboardInput {device_id, input}, ..} => {
+                let key_code = if let Some(keycode) = input.virtual_keycode {
+                    keycode as u8 + 8
+                } else {
+                    200
+                };
+
+                if previous_key_state[key_code as usize] != (input.state == ElementState::Pressed) {
+                    stream.send(&[key_code, input.state as u8]).expect("Failed to write");
+                    previous_key_state[key_code as usize] = (input.state == ElementState::Pressed);
+                    println!("{}", key_code);
+                }
+                winit::ControlFlow::Continue
+            },
+            _ => winit::ControlFlow::Continue,
         }
-    }
-
+    });
 }
+
+//fn other_thread(other_window: std::sync::Arc<std::sync::Mutex<i32>>) {
+//    loop {
+//        std::thread::sleep(std::time::Duration::from_millis(20));
+//        let mut cursor_pos = winapi::windef::POINT { x: 0, y: 0 };
+//        println!("Thread num: {}", other_window.lock().unwrap());
+//        unsafe {
+//            //let data = std::ptr::read(window.get_hwnd());
+//
+//            //std::ptr::write(temp, window.get_hwnd());
+//            //user32::SetForegroundWindow(window.get_hwnd() as *mut _);
+//            //user32::ShowWindow(window.get_hwnd() as *mut _, 6);
+//        }
+////        unsafe {
+////            if user32::GetCursorPos(&mut cursor_pos as winapi::windef::LPPOINT) != 0 {
+////                user32::SetCursorPos(500,500);
+////                user32::ShowCursor(0);
+////            }
+////        }
+////
+////user32::SetWindowsHookExA()
+////        for i in 1..255u8 {
+////            let mut is_current_key_down: bool;
+////            unsafe {
+////                is_current_key_down = user32::GetAsyncKeyState(i as i32) & (1 << 15) != 0;
+////            }
+////            if previous_key_state[i as usize] != is_current_key_down {
+////                previous_key_state[i as usize] = is_current_key_down;
+////                stream.send(&[i, is_current_key_down as u8]).expect("Failed to write");
+////            }
+////        }
+//    }
+//}
